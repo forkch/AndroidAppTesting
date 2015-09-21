68e9070b16eddc9208a46e163fe36d3c98ec66e5
Benjamin Müller <news@fork.ch>
2015-09-21 20:21:07 +0200

    using mockito for mocking the backend

--- app/src/androidTest/java/ch/fork/androidapptesting/app/DemoUITest.java
+++ app/src/androidTest/java/ch/fork/androidapptesting/app/DemoUITest.java
@@ -1,15 +1,27 @@
 package ch.fork.androidapptesting.app;
 
+import android.content.Intent;
 import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.google.common.collect.Lists;
+
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Matchers;
+import org.mockito.Mockito;
+
+import java.util.Date;
+import java.util.List;
 
 import ch.fork.androidapptesting.R;
+import ch.fork.androidapptesting.app.model.Event;
 import ch.fork.androidapptesting.app.ui.eventlist.EventListActivity;
+import rx.Observable;
+import rx.schedulers.Schedulers;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
@@ -27,11 +39,34 @@ import static org.hamcrest.core.AllOf.allOf;
 public class DemoUITest {
 
     @Rule
-    public final ActivityTestRule<EventListActivity> activityTestRule = new ActivityTestRule<>(
-            EventListActivity.class);
+    public final MockBackendRule mockBackendRule = new MockBackendRule();
 
     @Rule
-    public final MockBackendRule mockBackendRule = new MockBackendRule();
+    public final ActivityTestRule<EventListActivity> activityTestRule = new ActivityTestRule<>(
+            EventListActivity.class, true, false);
+
+
+    public static final Event THE_EVENT = new Event(1, "ZEDays 2015", "Stuttgart", "description",
+            new Date());
+
+    @Before
+    public void setupMocksAndStartActivity() {
+        final List<Event> events = Lists.newArrayList(THE_EVENT);
+        Mockito.when(mockBackendRule.getMockEventService()
+                                    .getAllEvents())
+               .thenReturn(
+                       Observable.just(events)
+                                 .subscribeOn(Schedulers.immediate())
+               );
+        Mockito.when(mockBackendRule.getMockEventService()
+                                    .getEvent(Matchers.any(Long.class)))
+               .thenReturn(
+                       Observable.just(THE_EVENT)
+                                 .subscribeOn(Schedulers.immediate())
+               );
+
+        activityTestRule.launchActivity(new Intent(Intent.ACTION_MAIN));
+    }
 
     @Test
     public void happy_path() {
--- app/src/androidTest/java/ch/fork/androidapptesting/app/MockBackendRule.java
+++ app/src/androidTest/java/ch/fork/androidapptesting/app/MockBackendRule.java
@@ -6,6 +6,7 @@ import android.support.test.espresso.core.deps.guava.collect.Lists;
 import org.junit.rules.TestRule;
 import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
+import org.mockito.Mockito;
 
 import java.util.Date;
 import java.util.List;
@@ -15,27 +16,14 @@ import ch.fork.androidapptesting.app.model.Event;
 import rx.Observable;
 import rx.schedulers.Schedulers;
 
+import static org.mockito.Mockito.when;
+
 /**
  * Created with love by fork on 20.09.15.
  */
 public class MockBackendRule implements TestRule {
 
-    public static final Event THE_EVENT = new Event(1, "ZEDays 2015", "Stuttgart", "description",
-            new Date());
-    private EventService mockEventService = new EventService() {
-        @Override
-        public Observable<List<Event>> getAllEvents() {
-            final List<Event> events = Lists.newArrayList(THE_EVENT);
-            return Observable.just(events)
-                             .subscribeOn(Schedulers.immediate());
-        }
-
-        @Override
-        public Observable<Event> getEvent(long eventId) {
-            return Observable.just(THE_EVENT)
-                             .subscribeOn(Schedulers.immediate());
-        }
-    };
+    private EventService mockEventService;
 
     public EventService getMockEventService() {
         return mockEventService;
@@ -58,6 +46,7 @@ public class MockBackendRule implements TestRule {
 
     private void initMockBackend() {
 
+        mockEventService = Mockito.mock(EventService.class);
         AndroidAppTestingApp application = AndroidAppTestingApp
                 .get(InstrumentationRegistry.getTargetContext());
         application.setEventService(mockEventService);
