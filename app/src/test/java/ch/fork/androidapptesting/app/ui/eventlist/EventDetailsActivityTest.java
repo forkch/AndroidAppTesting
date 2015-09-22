bbad81ab1da8939b8f0c20fc98ac146656136d13
Benjamin Müller <news@fork.ch>
2015-09-22 21:08:59 +0200

    EventDetailActivityTest robolectric

--- app/src/test/java/ch/fork/androidapptesting/app/ui/eventlist/EventDetailsActivityTest.java
+++ app/src/test/java/ch/fork/androidapptesting/app/ui/eventlist/EventDetailsActivityTest.java
@@ -1,13 +1,12 @@
 package ch.fork.androidapptesting.app.ui.eventlist;
 
 import android.content.Intent;
-import android.widget.TextView;
 
+import org.assertj.android.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricGradleTestRunner;
-import org.robolectric.RuntimeEnvironment;
 import org.robolectric.Shadows;
 import org.robolectric.annotation.Config;
 import org.robolectric.shadows.ShadowActivity;
@@ -20,8 +19,6 @@ import ch.fork.androidapptesting.app.DefaultConfig;
 import ch.fork.androidapptesting.app.model.Event;
 import ch.fork.androidapptesting.app.ui.eventdetails.EventDetailActivity;
 
-import static org.assertj.android.api.Assertions.assertThat;
-
 
 /**
  * Created with love by fork on 17.09.15.
@@ -32,13 +29,22 @@ public class EventDetailsActivityTest {
 
     @Test
     public void when_event_location_is_clicked_it_should_start_intent_to_google_maps() {
-        // TODO
         // given
+        final EventDetailActivity eventDetailActivity = Robolectric.buildActivity(
+                EventDetailActivity.class)
+                                                               .create()
+                                                               .get();
 
         // when
+        eventDetailActivity.setEvent(new Event(1, "ZEDays 2015", "ICS Stuttgart", "", new Date()));
+        eventDetailActivity.findViewById(R.id.tvLocation).callOnClick();
 
         // then
+        final ShadowActivity shadowActivity = Shadows.shadowOf(eventDetailActivity);
+        final Intent capturedIntent = shadowActivity.getNextStartedActivity();
 
+        Assertions.assertThat(capturedIntent).hasAction(Intent.ACTION_VIEW)
+                .hasData("geo:0,0?q=ICS Stuttgart");
     }
 
 }
